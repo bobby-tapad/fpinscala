cbe9535ef3c4c4cb3b114771fb44eb52c6e56fc9
Bobby Victory <bobby.victory@tapad.com>
2020-12-23 13:37:49 -0500

    State

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -33,53 +33,114 @@ object RNG {
   // 6.1
   // Use RNG.nextInt to generate a random number between 0 and Int.MaxValue inclusive.  Corner case
   // when nextInt returns Int.MinValue, which doesn't have a negative counterpart
-  def nonNegativeInt(rng: RNG): (Int, RNG) = ???
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (i, r) = rng.nextInt
+    if (i < 0) (-i + 1, r) else (i, r)
+  }
 
   // 6.2
   // Generate a double between 0 and 1, not including 1.  Use Int.MaxValue and _.toDouble
-  def double(rng: RNG): (Double, RNG) = ???
+  def double(rng: RNG): (Double, RNG) = {
+    val (i, r) = nonNegativeInt(rng)
+    (i / (Int.MaxValue.toDouble + 1), r)
+  }
 
   // 6.3 use functions already written
-  def intDouble(rng: RNG): ((Int,Double), RNG) = ???
+  def intDouble(rng: RNG): ((Int,Double), RNG) = {
+    val (i, r) = rng.nextInt
+    val (d, r2) = double(r)
+    ((i, d), r2)
+  }
 
   // 6.3 use functions already written
-  def doubleInt(rng: RNG): ((Double,Int), RNG) = ???
+  def doubleInt(rng: RNG): ((Double,Int), RNG) = {
+    val ((i, d), r) = intDouble(rng)
+    ((d, i), r)
+  }
 
   // 6.3 use functions already written
-  def double3(rng: RNG): ((Double,Double,Double), RNG) = ???
+  def double3(rng: RNG): ((Double,Double,Double), RNG) = {
+    val (d1, r1) = double(rng)
+    val (d2, r2) = double(r1)
+    val (d3, r3) = double(r2)
+    ((d1, d2, d3), r3)
+  }
 
   // 6.4 list of random Ints
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    if (count <= 0) (Nil, rng)
+    else {
+      val (i, r1) = rng.nextInt
+      val (l, r2) = ints(count - 1)(r1)
+      (i :: l, r2)
+    }
+  }
 
   // 6.5 use map to implement double
+  def doubleMap: Rand[Double] = map(nonNegativeInt)(i => i / (Int.MaxValue.toDouble + 1))
 
   // 6.6
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = rng => {
+    val (a, r2) = ra(rng)
+    val (b, r3) = rb(r2)
+    (f(a, b), r3)
+  }
 
   // 6.7
-  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = {
+    fs.foldRight(unit(List[A]()))((ra, rb) => map2(ra, rb)(_ :: _))
+  }
 
   // 6.7 use sequence and List.fill(n)(x) to implement ints
+  def ints2(count: Int): Rand[List[Int]] = {
+    sequence(List.fill(count)(int))
+  }
 
   // 6.8
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
+  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = rng => {
+    val (a, r) = f(rng)
+    g(a)(r)
+  }
 
   // 6.8 use flatMap to implement nonNegativeLessThan
+  def nonNegativeLessThan(n: Int): Rand[Int] = flatMap(nonNegativeInt) {
+    i =>
+      val mod = i % n
+      if (i + (n - 1) - mod >= 0) unit(mod) else nonNegativeLessThan(n)
+  }
 
   // 6.9 reimplement map and map2 in terms of flatmap
+  def mapfm[A,B](s: Rand[A])(f: A => B): Rand[B] = {
+    flatMap(s)(a => unit(f(a)))
+  }
+
+  def map2fm[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+    flatMap(ra)(a => map(rb)(b => f(a, b)))
+  }
+
 }
 
 // 6.10 Generalize the functions unit, map, map2, flatMap, and sequence
 // on the State case class where possible, otherwise on the companion object
 
 case class State[S,+A](run: S => (A, S)) {
-  def map[B](f: A => B): State[S, B] =
-    ???
-  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
-    ???
-  def flatMap[B](f: A => State[S, B]): State[S, B] =
-    ???
+  def map[B](f: A => B): State[S, B] = flatMap(a => unit(f(a)))
+
+  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] = flatMap(a => sb.map(b => f(a, b)))
+
+  def flatMap[B](f: A => State[S, B]): State[S, B] = State(s1 => {
+    val (a, s2) = run(s1)
+    f(a).run(s2)
+  })
+
+  def sequence[S,A](fs: List[State[S,A]]): State[S,List[A]] = {
+    fs.foldRight(unit[S, List[A]](List[A]()))((sa, sb) => sa.map2(sb)(_ :: _))
+  }
+
+  def unit[S,A](a: A): State[S,A] = State(s => (a, s))
+
 }
+
 
 sealed trait Input
 case object Coin extends Input
@@ -89,11 +150,32 @@ case class Machine(locked: Boolean, candies: Int, coins: Int)
 
 object State {
   type Rand[A] = State[RNG, A]
-  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = ???
 
   // Inserting a coin into a locked machine will cause it to unlock
   // Turning the knob on an unlocked machine will dispense candy and become locked
   // Turning the knob on a locked machine or inserting a coin in an unlocked machine does nothing
   // A machine with no candy ignores all inputs
-  // need methods get, set, modify
+
+  def simulateMachine(inputs: List[Input]): State[Machine, (Int, Int)] = {
+
+    def resolveInputs(inputs: List[Input], machine: Machine): Machine = inputs match {
+      case Nil => machine
+      case input :: t => resolveInputs(t, resolveInput(input, machine))
+    }
+
+    def resolveInput(input: Input, machine:Machine): Machine = (input, machine) match {
+      case (_, Machine(_, 0, _)) => machine
+      case (Coin, Machine(true, candies, coins)) => Machine(false, candies, coins + 1)
+      case (Turn, Machine(false, candies, coins)) => Machine(true, candies - 1, coins)
+      case _ => machine
+    }
+
+    def run: Machine => ((Int, Int), Machine) = (machine: Machine) => {
+      val m2 = resolveInputs(inputs, machine)
+      ((m2.coins, m2.candies), m2)
+    }
+
+    State(run)
+  }
+
 }
--- exercises/src/test/scala/fpinscala/StateSpec.scala
+++ exercises/src/test/scala/fpinscala/StateSpec.scala
@@ -1,6 +1,7 @@
 package fpinscala
 
-import fpinscala.state.{ RNG }
+import fpinscala.state.State.simulateMachine
+import fpinscala.state.{Coin, Machine, RNG, State, Turn}
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 
@@ -9,5 +10,48 @@ class StateSpec extends AnyFlatSpec with Matchers {
   it should "sequence" in {
     val r = RNG.Simple(42)
     RNG.sequence(List(RNG.unit(1), RNG.unit(2), RNG.unit(3)))(r)._1 shouldBe List(1, 2, 3)
+  }
+
+  it should "Inserting a coin into a locked machine will cause it to unlock" in {
+    val m1: Machine = Machine(locked = true, candies = 1, coins = 1)
+    val inputs = List(Coin)
+
+    State.simulateMachine(inputs).run(m1) shouldBe ((2, 1), Machine(false, 1, 2))
+  }
+
+  it should "Turning the knob on an unlocked machine will dispense candy and become locked" in {
+    val m1: Machine = Machine(locked = false, candies = 1, coins = 1)
+    val inputs = List(Turn)
+
+    State.simulateMachine(inputs).run(m1) shouldBe ((1, 0), Machine(true, 0, 1))
+  }
+
+  it should "Turning the knob on a locked machine does nothing" in {
+    val m1: Machine = Machine(locked = true, candies = 1, coins = 1)
+    val inputs = List(Turn)
+
+    State.simulateMachine(inputs).run(m1) shouldBe ((1, 1), Machine(true, 1, 1))
+  }
+
+  it should "Inserting a coin in an unlocked machine does nothing" in {
+    //I am assuming "does nothing" means the machine does not accept the coin
+    val m1: Machine = Machine(locked = false, candies = 1, coins = 1)
+    val inputs = List(Coin)
+
+    State.simulateMachine(inputs).run(m1) shouldBe ((1, 1), Machine(false, 1, 1))
+  }
+
+  it should "A machine with no candy ignores all inputs" in {
+    val m1: Machine = Machine(locked = true, candies = 0, coins = 1)
+    val inputs = List(Turn, Coin, Turn, Turn, Coin)
+
+    State.simulateMachine(inputs).run(m1) shouldBe ((1, 0), Machine(true,0, 1))
+  }
+
+  it should "do the example from the book" in {
+    val m1: Machine = Machine(true, 5, 10)
+    val inputs = List(Coin, Turn, Coin, Turn, Turn, Coin, Turn, Coin, Turn)
+
+    State.simulateMachine(inputs).run(m1) shouldBe ((14, 1), Machine(true, 1, 14))
   }
 }
